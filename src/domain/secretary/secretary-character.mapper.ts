b5f9124ebb8eb76c95876b0001f033a0434ef80f
forntonio <anton_fors@hotmail.com>
2025-08-24 13:48:54 -0700

    clarify secretary character map typing

--- src/domain/secretary/secretary-character.mapper.ts
+++ src/domain/secretary/secretary-character.mapper.ts
@@ -1,7 +1,6 @@
-import type { Card } from '../cards.type';
-import { type FrosthavenClass, type FrosthavenClassNames } from '../frosthaven-class.type';
+import { type FrosthavenClassNames } from '../frosthaven-class.type';
 
-export function mapCharacterNameToSecretary(name: FrosthavenClass<Card>['name']) {
+export function mapCharacterNameToSecretary(name: FrosthavenClassNames) {
   const map: Record<FrosthavenClassNames, string> = {
     'Banner Spear': 'banner-spear',
     'Bladeswarm': 'envx',
